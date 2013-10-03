a306bda9d633b488ba448fb52c2d6f1448e9d7a8
moonglum <moonglum@moonbeamlabs.com>
2013-10-03 19:02:43 +0200

    Introducing Centered Row

---
+++ lib/megingiard/centered_row.rb
@@ -0,0 +1,17 @@
+require 'megingiard/terminal_width'
+
+module Megingiard
+  # A full-width row where the content is centered
+  class CenteredRow
+    def initialize(node, output, terminal_width)
+      @node = node
+      @output = output
+      @terminal_width = terminal_width
+    end
+
+    def draw
+      centered_text = @node.to_s.center(@terminal_width)
+      @output.puts centered_text
+    end
+  end
+end
---
+++ spec/unit/centered_row/draw_spec.rb
@@ -0,0 +1,32 @@
+require 'megingiard/centered_row'
+
+describe Megingiard::CenteredRow do
+  subject { Megingiard::CenteredRow.new(node, output, terminal_width) }
+  let(:node) { double }
+  let(:node_as_string) { double }
+  let(:centered_text) { double }
+  let(:output) { double }
+  let(:terminal_width) { double }
+
+  before do
+    allow(node).to receive(:to_s)
+      .and_return(node_as_string)
+    allow(node_as_string).to receive(:center)
+      .and_return(centered_text)
+    allow(output).to receive(:puts)
+  end
+
+  describe 'draw' do
+    it 'should to_s and center the text' do
+      expect(node_as_string).to receive(:center)
+        .with(terminal_width)
+      subject.draw
+    end
+
+    it 'should put the resulting text to the output' do
+      expect(output).to receive(:puts)
+        .with(centered_text)
+      subject.draw
+    end
+  end
+end
