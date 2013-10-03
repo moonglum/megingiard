117a81ba9261441ce7c00c6df585e67086f1e546
moonglum <moonglum@moonbeamlabs.com>
2013-10-03 19:20:45 +0200

    Extracted Canvas

---
+++ lib/megingiard/canvas.rb
@@ -0,0 +1,6 @@
+# -*- encoding : utf-8 -*-
+
+module Megingiard
+  # An area that can be drawn on
+  Canvas = Struct.new(:node, :output, :terminal_width)
+end
--- lib/megingiard/centered_row.rb
+++ lib/megingiard/centered_row.rb
@@ -1,18 +1,12 @@
 # -*- encoding : utf-8 -*-
-require 'megingiard/terminal_width'
+require 'megingiard/canvas'
 
 module Megingiard
   # A full-width row where the content is centered
-  class CenteredRow
-    def initialize(node, output, terminal_width)
-      @node = node
-      @output = output
-      @terminal_width = terminal_width
-    end
-
+  class CenteredRow < Canvas
     def draw
-      centered_text = @node.to_s.center(@terminal_width)
-      @output.puts centered_text
+      centered_text = node.to_s.center(terminal_width)
+      output.puts centered_text
     end
   end
 end
